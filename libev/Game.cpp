23880ac2328c0dcd88fddb600afef703647c62d8
Jesse Deisinger <jett925@gmail.com>
2016-06-25 15:31:14 -0400

    Switched to using map to store info

--- libev/Game.cpp
+++ libev/Game.cpp
@@ -7,64 +7,20 @@
 namespace EvLib {
 	Game::Game(std::string gid) {
 		id = gid;
+		std::map<Info, std::string> info;
 	}
-	void Game::setInfo(std::map< Info, std::string > info) {
-
+	void Game::setInfo(std::map< Info, std::string > ninfo) {
+		info = std::map<Info, std::string>(ninfo);
 	}
-	std::string Game::getInfo(Info info) {
-		switch (info) {
-		case Info::visteam:
-			return visteam;
-		case Info::hometeam:
-			return hometeam;
-		case Info::site:
-			return site;
-		case Info::date:
-			return date;
-		case Info::number:
-			return number;
-		case Info::starttime:
-			return starttime;
-		case Info::daynight:
-			return daynight;
-		case Info::usedh:
-			return usedh;
-		case Info::umphome:
-			return umphome;
-		case Info::ump1b:
-			return ump1b;
-		case Info::ump2b:
-			return ump2b;
-		case Info::ump3b:
-			return ump3b;
-		case Info::howscored:
-			return howscored;
-		case Info::pitches:
-			return pitches;
-		case Info::temp:
-			return temp;
-		case Info::winddir:
-			return winddir;
-		case Info::windspeed:
-			return windspeed;
-		case Info::fieldcond:
-			return fieldcond;
-		case Info::precip:
-			return precip;
-		case Info::sky:
-			return sky;
-		case Info::timeofgame:
-			return timeofgame;
-		case Info::attendance:
-			return attendance;
-		case Info::wp:
-			return wp;
-		case Info::lp:
-			return lp;
-		case Info::save:
-			return save;
-
-		default:
+	std::map<Game::Info, std::string> Game::getInfo(void) {
+		return std::map<Info, std::string>(info);
+	}
+	std::string Game::getInfo(Info i) {
+		try {
+			return info.at(i);
+		}
+		catch (const std::out_of_range& oor) {
+			std::cerr << "Out of Range error: " << oor.what() << '\n';
 			return NULL;
 		}
 	}
--- libev/Game.h
+++ libev/Game.h
@@ -19,17 +19,11 @@ namespace EvLib {
 		};
 		Game(std::string gid);
 		std::string getInfo(Info info);
+		std::map<Info, std::string> getInfo(void);
 		void setInfo(std::map< Info, std::string > info);
 
 	private:
-		std::string id, visteam, hometeam, site,
-			date, number, starttime,
-			daynight, usedh, umphome,
-			ump1b, ump2b, ump3b,
-			howscored, pitches, temp,
-			winddir, windspeed, fieldcond,
-			precip, sky, timeofgame,
-			attendance, wp, lp,
-			save;
+		std::string id;
+		std::map < Info, std::string> info;
 	};
 }
